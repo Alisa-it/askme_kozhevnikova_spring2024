65be6c833ac59a2293c819e7e2cfa6d517d0e8d3
Alisa Kozhevnikova <askerrr2002@gmail.com>
2024-04-08 21:19:54 +0300

    Added templates, pagination and flexible routing

--- app/urls.py
+++ app/urls.py
@@ -5,10 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('settings/', views.settings, name='settings'),
     path('ask/', views.ask, name='ask'),
-    path('question/', views.question, name='question'),
+    path('question/<int:question_id>', views.question, name='question'),
     path('login/', views.login, name='login'),
-    path('register/', views.register, name='signup'),
-    path('tag/', views.tag, name='tag'),
+    path('signup/', views.register, name='signup'),
+    path('tag/<str:tag_name>', views.tag, name='tag'),
     path('hot/', views.hot, name='hot'),
-
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,39 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+QUESTIONS = [
+    {
+        "title": f"Question {i}",
+        "text": f"This is question number {i}",
+        "id": i,
+    }
+    for i in range(95)
+]
+ANSWERS = [
+    {
+        "text": f"This is very interesting answer number {i}",
+    }
+    for i in range(5)
+]
+
+TAGS = [
+    {
+        "name": tag,
+    }
+    for tag in ['blabla', 'tag', 'meme']
+]
+
+
+def paginate(request, objects_list, per_page):
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(objects_list, per_page)
+    obj = paginator.page(page_num)
+    return obj
 
 
 def index(request):
-    return render(request, "index.html")
+    page_obj = paginate(request, QUESTIONS, 20)
+    return render(request, "index.html", {"questions": page_obj, "tags": TAGS})
 
 
 def settings(request):
@@ -13,8 +44,9 @@ def ask(request):
     return render(request, "ask.html")
 
 
-def question(request):
-    return render(request, "question.html")
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, "question.html", {"questions": item, "answers": ANSWERS, "tags": TAGS})
 
 
 def login(request):
@@ -25,9 +57,11 @@ def register(request):
     return render(request, "signup.html")
 
 
-def tag(request):
-    return render(request, "tag.html")
+def tag(request, tag_name):
+    page_obj = paginate(request, QUESTIONS[10:], 10)
+    return render(request, "tag.html", {"questions": page_obj, "tag": tag_name, "tags": TAGS})
 
 
 def hot(request):
-    return render(request, "hot.html")
+    page_obj = paginate(request, QUESTIONS[18:], 10)
+    return render(request, "hot.html", {"questions": page_obj, "tags": TAGS})
